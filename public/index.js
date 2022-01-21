29d009e621d27252c9f2c4a8645b8e7feadf03e4
Ezequiel Tamez Lopez <ezequiel.tamez@uttn.mx>
2022-01-20 21:11:44 -0600

    Update

--- public/index.js
+++ public/index.js
@@ -12,14 +12,18 @@ let students = [
 }
 ]
 
+let table = document.getElementsByTagName("table")[0];
 for(let i = 0; students.length >i;i++){
-    let templ = document.getElementById("tbodytempl").content.clonenode(true);
-    let tdnumber = templ.querySelector('span[name=tbodyNumber]')
-    let tdname = templ.querySelector('span[name=tbodyName]')
-    let tdage = templ.querySelector('span[name=tbodyAge]')
-    let tdcareer = templ.querySelector('span[name=tbodyCareer]')
+    let templ = document.getElementById('tbodytempl').content
+    let tdnumber = templ.querySelector('span[name=tbodyNumber]');
+    let tdname = templ.querySelector('span[name=tbodyName]');
+    let tdage = templ.querySelector('span[name=tbodyAge]');
+    let tdcareer = templ.querySelector('span[name=tbodyCareer]');
 
     tdnumber.innerText= i+1;
-    tdage.innerText=students[i].lname + " " + students[i].name;
+    tdname.innerText = students[i].lname + " " + students[i].name;
+    tdage.innerText=students[i].age;
     tdcareer.innerText= students[i].career;
+
+    table.append(templ);
 }
